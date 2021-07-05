e84a7db6a0e4f44472ab310bef5ddb4c32771eb9
supertree-emjay <emjay@supertree.co>
2021-07-05 18:07:33 +0900

    e2e(with cypress) 테스트 코드 작성

---
+++ cypress/integration/App.e2e.js
@@ -0,0 +1,21 @@
+/* eslint-disable no-undef */
+
+describe("App E2E", () => {
+  it("헤더가 있어야 한다", () => {
+    cy.visit("/");
+    cy.get("h1").should("have.text", "My Counter");
+  });
+
+  it("카운터를 늘리거나 줄일수 있어야 한다", () => {
+    cy.visit("/");
+    cy.get("p").should("have.text", "0");
+    cy.contains("Increment").click();
+    cy.get("p").should("have.text", "1");
+    cy.contains("Increment").click();
+    cy.get("p").should("have.text", "2");
+    cy.contains("Decrement").click();
+    cy.get("p").should("have.text", "1");
+    cy.contains("Decrement").click();
+    cy.get("p").should("have.text", "0");
+  });
+});
