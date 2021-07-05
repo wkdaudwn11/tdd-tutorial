ba9dcebe7c6d0a426beeee0fbd6a420284388ec7
supertree-emjay <emjay@supertree.co>
2021-07-05 14:08:03 +0900

    Feat: Update App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from "./logo.svg";
-import "./App.css";
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export const doIncrement = (prevState) => ({
+  counter: prevState.counter + 1,
+});
+
+export const doDecrement = (prevState) => ({
+  counter: prevState.counter - 1,
+});
+
+export const Counter = ({ counter }) => <p>{counter}</p>;
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      counter: 0,
+    };
+    this.onIncrement = this.onIncrement.bind(this);
+    this.onDecrement = this.onDecrement.bind(this);
+  }
+
+  onIncrement() {
+    this.setState(doIncrement);
+  }
+
+  onDecrement() {
+    this.setState(doDecrement);
+  }
+
+  render() {
+    const { counter } = this.state;
+    return (
+      <div>
+        <h1>My Counter</h1>
+
+        <Counter counter={counter} />
+
+        <button type="button" onClick={this.onIncrement}>
+          Increment
+        </button>
+
+        <button type="button" onClick={this.onDecrement}>
+          Decrement
+        </button>
+      </div>
+    );
+  }
 }
 
 export default App;
